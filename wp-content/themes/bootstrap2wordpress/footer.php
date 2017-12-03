f5efe89e2397f8cc9bd012865d71564678e72e77
msm3921 <msm3921@yahoo.com>
2017-12-03 15:05:44 -0500

    been a while
    header/footer done. on lecture 47

--- wp-content/themes/bootstrap2wordpress/footer.php
+++ wp-content/themes/bootstrap2wordpress/footer.php
@@ -11,24 +11,20 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap2wordpress' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'bootstrap2wordpress' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bootstrap2wordpress' ), 'bootstrap2wordpress', '<a href="http://underscores.me/">Matt Mueller</a>' );
-			?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
+
+<!-- Took this portion from Brad's static site -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="<?php bloginfo('template_directory')?>/assets/js/jquery-2.1.1.min.js"></script>
+    <script src="<?php bloginfo('template_directory')?>/assets/js/bootstrap.min.js"></script>
+    
+    <script src="<?php bloginfo('template_directory')?>/assets/js/main.js"></script>
+    
+    <script type="<?php bloginfo('template_directory')?>/text/javascript" src="//use.typekit.net/gla7wnd.js"></script>
+	<script type="<?php bloginfo('template_directory')?>/text/javascript">try{Typekit.load();}catch(e){}</script>
 
 </body>
 </html>
--- wp-content/themes/bootstrap2wordpress/header.php
+++ wp-content/themes/bootstrap2wordpress/header.php
@@ -16,9 +16,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	
+	<!--**** MSM ****-->
 	<!-- Bootstrap -->
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory');?>/assets/css/bootstrap.min.css">
 	<!-- Font Icons -->
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome/css/font-awesome.min.css">
 	<!-- Google Fonts -->
@@ -29,7 +29,7 @@
 	<!--[if lt IE 9] -->
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+	<!--[endif]-->
 </head>
 
 <body <?php body_class(); ?>>
@@ -50,13 +50,15 @@
 								<span class="icon-bar"></span>
 							</button>
 							<a class="navbar-brand"href="/">
-								<img src="assets/img/logo.png">
+								<img src="<?php bloginfo('stylesheet_directory');?>/assets/img/logo.png"
+								 alt="Bootstrap to WordPress"
+								>
 							</a>
 						</div> <!--Navbar header-->
 						
 						<?php
 							wp_nav_menu( array(
-								'theme_location'    => 'primary',
+								'theme_location'    => 'menu-1', /* see register_nav_menus() in functions.php */
 								'container'         => 'nav',
 								'container_class'   => 'navbar-collapse collapse',
 								'menu_class'        => 'nav navbar-nav navbar-right' 
